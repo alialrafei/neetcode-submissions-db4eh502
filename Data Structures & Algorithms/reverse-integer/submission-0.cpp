a4000147ff127ffb67e6e12e087ac77273d4a17c
Ali Al rafei <42154249+alialrafei@users.noreply.github.com>
2026-05-25 22:39:23 +0300

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int number = 0;
+        while(x!=0){
+            int digit = x%10;
+            if((number > INT_MAX/10 ) || (number == INT_MAX/10 && digit > 7))
+                return 0;
+            if (number < INT_MIN / 10 || 
+           (number == INT_MIN / 10 && digit < -8))
+            return 0;
+
+            number = number * 10 + digit;
+            x/=10;
+        }
+        return number;
+    }
+};
