4c54ed97854fb309dd08f47f4e651424fba002dd
Ali Al rafei <42154249+alialrafei@users.noreply.github.com>
2026-05-30 15:21:22 +0300

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int current_end = 0;
+        int farthest = 0;
+        int n = nums.size();
+        int jumps = 0;
+        for (int i = 0; i < n - 1; i++) {
+            farthest = max(farthest, i + nums[i]);
+            if (i == current_end) {
+                jumps++;
+                current_end = farthest;
+            }
+        }
+        return jumps;
+    }
+};
